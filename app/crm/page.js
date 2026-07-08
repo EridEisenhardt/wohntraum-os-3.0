d62b0fdc2da71a99358aabd20a94b0f754ad53ba
Eric Eisenhardt <eric@wohntraumrheinhessen.site>
2026-07-08 19:31:46 +0200

    feat(crm): CRM nach Zoho-Vorbild (Kontakte, Firmen, Deals-Pipeline mit Drag&Drop, Aktivitaeten, Zeitstrahl je Datensatz inkl. E-Mails); Sidebar unter Vertrieb

---
+++ app/crm/page.js
@@ -0,0 +1,12 @@
+'use client'
+export default function CrmPage() {
+  return (
+    <div style={{ margin: '-26px -30px', height: '100vh' }}>
+      <iframe
+        src="/crm.html"
+        title="CRM"
+        style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
+      />
+    </div>
+  )
+}
