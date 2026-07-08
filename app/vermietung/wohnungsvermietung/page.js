8c49761e6e622ddf19718c4ff39a78abd693836f
Eric Eisenhardt <eric@wohntraumrheinhessen.site>
2026-07-08 09:47:16 +0200

    feat(vermietung): Wohnungsvermietung-Prozess (Stufen 1-8 + Warteliste/Reno/Leerstand); Gebaeude/Einheit-Dropdowns (erweiterbar), Grund/Zielgruppe/Ein-/Auszug/Freigabe, Interessenten-Verknuepfung (Tags) zu Mietinteressenten

---
+++ app/vermietung/wohnungsvermietung/page.js
@@ -0,0 +1,12 @@
+'use client'
+export default function WohnungsvermietungPage() {
+  return (
+    <div style={{ margin: '-26px -30px', height: '100vh' }}>
+      <iframe
+        src="/wohnungsvermietung.html"
+        title="Vermietung · Wohnungsvermietung"
+        style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
+      />
+    </div>
+  )
+}
