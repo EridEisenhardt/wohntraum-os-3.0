ddbdd9ff18492b5afd6d0e602bccf49a837bdf5a
Eric Eisenhardt <eric@wohntraumrheinhessen.site>
2026-08-03 18:56:42 +0200

    Hausverwaltung: neue Unterkategorie Mietermeldungen - SOP/Prozess-Dokumentation fuer 13 Standardfaelle (editierbar, weiterentwickelbar)

---
+++ app/hausverwaltung/mietermeldungen/page.js
@@ -0,0 +1,12 @@
+'use client'
+export default function MietermeldungenPage() {
+  return (
+    <div style={{ margin: '-26px -30px', height: '100vh' }}>
+      <iframe
+        src="/mietermeldungen.html"
+        title="Mietermeldungen"
+        style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
+      />
+    </div>
+  )
+}
