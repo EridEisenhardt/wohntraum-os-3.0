834fda9af14dd21d3fba50c54a7eaa488f577c1d
Eric Eisenhardt <eric@wohntraumrheinhessen.site>
2026-08-10 16:48:41 +0200

    Geschaeftsfuehrer: Unterkategorie Persoenliche Assistenz mit Osteopath-Bereich (Ralf Braun Undenheim, Akuttermin-Link praxisarchiv24.de, Terminliste)

---
+++ app/persoenliche-assistenz/page.js
@@ -0,0 +1,12 @@
+'use client'
+export default function PersoenlicheAssistenzPage() {
+  return (
+    <div style={{ margin: '-26px -30px', height: '100vh' }}>
+      <iframe
+        src="/persoenliche-assistenz.html"
+        title="Persönliche Assistenz"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+      />
+    </div>
+  )
+}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -123,6 +123,7 @@ export const NAV = [
     { href: '/eric-privat/kontenmodell', icon: 'ti-wallet', label: '6 Kontenmodell' },
   ] },
   { type: 'link', href: '/jahresplaner', icon: 'ti-calendar', label: 'Jahresplaner', area: ['vertrieb', 'hv'], mod: 'privat' },
+  { type: 'link', href: '/persoenliche-assistenz', icon: 'ti-user-heart', label: 'Persönliche Assistenz', area: ['vertrieb', 'hv'], mod: 'privat' },
   { type: 'link', href: '/haushaltshilfe', icon: 'ti-home-heart', label: 'Haushaltshilfe', area: ['vertrieb', 'hv'], mod: 'privat' },
   { type: 'link', href: '/konto', icon: 'ti-user-cog', label: 'Mein Konto', area: 'common' },
   { type: 'link', href: '/nutzer', icon: 'ti-shield-lock', label: 'Nutzerverwaltung', area: 'common', mod: 'nutzer' },
@@ -160,7 +161,7 @@ export const GB_MAP = {
   hausverwaltung: 'hausverwaltung', prozesse: 'hausverwaltung',
   produktivitaet: 'gf', personal: 'gf', 'eric-privat': 'gf',
   'gf-dashboard': 'gf', '/gf-dashboard': 'gf', '/beschluesse': 'gf', '/aktivitaeten': 'hausverwaltung', '/dokumente': 'buchhaltung', '/nutzer': 'gf',
-  '/hausmeisterdienst': 'hausmeisterdienst', '/haushaltshilfe': 'gf', '/jahresplaner': 'gf',
+  '/hausmeisterdienst': 'hausmeisterdienst', '/haushaltshilfe': 'gf', '/jahresplaner': 'gf', '/persoenliche-assistenz': 'gf',
 }
 export const gbOf = (n) => (n.type === 'group' ? GB_MAP[n.key] : GB_MAP[n.href])
 
